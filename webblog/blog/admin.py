c8189b01c7272925da38d6e6712955b44c3d9f40
Ali Akbar <52413434+Ali-D-Akbar@users.noreply.github.com>
2019-09-26 13:13:34 +0500

    Revert "moving webblog"

--- webblog/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from blog.models import Blog, Comment
-
-
-admin.site.register(Blog)
-admin.site.register(Comment)
--- webblog/webblog/settings/production.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-DEBUG = False
-INSTALLED_APPS += (
-    # other apps for production site
-)
