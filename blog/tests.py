23ce338143c11a69dccfd8b26e5a92cb4bf2e78e
Ali-D-Akbar <AliAkbarIT019@gmail.com>
2019-10-24 15:37:02 +0500

    100% coverage achieved

--- blog/tests.py
+++ blog/tests.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIRequestFactory, APITestCase, force_authentica
 
 from accounts.views import RegisterAPI
 from blog.models import Blog
-from blog.views import BlogAPI
+from blog.views import BlogAPI, CommentAPI
 
 pytestmark = pytest.mark.django_db
 
@@ -14,18 +14,21 @@ class BlogTests(APITestCase):
     """
     Tests for Blog.
     """
+    factory = APIRequestFactory()
+
     def register(self, credentials):
         """
         Registers a user given the credentials for test purposes.
         """
-        factory = APIRequestFactory()
         view = RegisterAPI.as_view()
         url = '/api/auth/register'
 
-        request = factory.post(url, credentials)
-        return view(request)
+        request = self.factory.post(url, credentials)
+        response = view(request)
+        assert status.HTTP_200_OK == response.status_code
+        return response
 
-    def create_blog_request(self, body):
+    def create_blog_request(self, create_blog_content):
         """
         Creates a Blog Post.
         """
@@ -34,14 +37,26 @@ class BlogTests(APITestCase):
             'email': 'abc@example.com',
             'password': '123'
         }
-
         self.register(credentials)
-
-        factory = APIRequestFactory()
         url = '/api/blog'
-
-        request = factory.post(url, body)
+        request = self.factory.post(url, create_blog_content)
         return request
+
+    def _create_blog(self):
+        create_blog_content = {
+            'title': 'Test',
+            'description': 'This is a Test Blog.'
+        }
+        request = self.create_blog_request(create_blog_content)
+        user = User.objects.get(username='test')
+        view = BlogAPI.as_view({
+            'post': 'create'
+        })
+        force_authenticate(request, user=user)
+        response = view(request)
+
+        assert status.HTTP_201_CREATED == response.status_code
+        return response
 
     def test_get_blog_list_authorized(self):
         """
@@ -52,89 +67,201 @@ class BlogTests(APITestCase):
             'email': 'abc@example.com',
             'password': '123'
         }
+        self.register(credentials)
 
-        response = self.register(credentials)
-
-        factory = APIRequestFactory()
         view = BlogAPI.as_view({
             'get': 'list'
         })
         url = '/api/blog'
         user = User.objects.get(username='test')
-
-        request = factory.get(url)
+        request = self.factory.get(url)
         force_authenticate(request, user=user)
         response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        assert status.HTTP_200_OK == response.status_code
 
     def test_create_blog(self):
         """
         Tests to create a Blog Post Successfully.
         """
-        body = {
-            'title': 'Test',
-            'description': 'This is a Test Blog.'
-        }
-        request = self.create_blog_request(body)
-        user = User.objects.get(username='test')
-        view = BlogAPI.as_view({
-            'post': 'create'
-        })
+        response = self._create_blog()
 
-        force_authenticate(request, user=user)
-        response = view(request)
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Blog.objects.count(), 1)
-        self.assertEqual(Blog.objects.get().title, 'Test')
+        assert status.HTTP_201_CREATED == response.status_code
+        assert 1 == Blog.objects.count()
+        assert 'Test' == Blog.objects.get().title
 
     def test_create_blog_failed(self):
         """
         Tests to create a Blog Post Unsuccessfully.
         """
-        body = {
+        create_blog_content = {
             'title': 'This is a Test Blog.'
         }
         view = BlogAPI.as_view({
             'post': 'create'
         })
-        request = self.create_blog_request(body)
+        request = self.create_blog_request(create_blog_content)
         user = User.objects.get(username='test')
-
         force_authenticate(request, user=user)
         response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        assert status.HTTP_400_BAD_REQUEST == response.status_code
 
     def test_delete_blog(self):
         """
         Tests to delete a Blog Post.
         """
-        body = {
-            'title': 'Test',
-            'description': 'This is a Test Blog to be deleted.'
-        }
-        request = self.create_blog_request(body)
+        self._create_blog()
         user = User.objects.get(username='test')
-        view = BlogAPI.as_view({
-            'post': 'create'
-        })
-
-        force_authenticate(request, user=user)
-        response = view(request)
 
         blog = Blog.objects.get()
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Blog.objects.count(), 1)
-        self.assertEqual(Blog.objects.get().title, 'Test')
-
         view = BlogAPI.as_view({
             'delete': 'destroy'
         })
         url = '/api/blog'
-        factory = APIRequestFactory()
-        request = factory.delete(url)
-
+        request = self.factory.delete(url)
         force_authenticate(request, user=user)
         response = view(request, slug=blog.slug)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        assert status.HTTP_204_NO_CONTENT == response.status_code
+
+    def test_update_blog(self):
+        """
+        Tests to update a Blog Post Successfully.
+        """
+        self._create_blog()
+        user = User.objects.get(username='test')
+
+        blog = Blog.objects.get()
+        update_blog_content = {
+            'title': 'Test101',
+            'description': 'This is a Test Blog. Amirite! :D'
+        }
+        url = '/api/blog/'
+        request = self.factory.put(url, update_blog_content)
+        view = BlogAPI.as_view({
+            'put': 'update'
+        })
+        force_authenticate(request, user=user)
+        response = view(request, slug=blog.slug)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 1 == Blog.objects.count()
+        assert update_blog_content['title'] == Blog.objects.get().title
+
+    def test_search_blog_list(self):
+        """
+        Tests to search Blog Posts Successfully.
+        """
+        self._create_blog()
+
+        keyword = 'Test'
+        url = '/api/blog/?keyword=%s' % keyword
+        request = self.factory.get(url)
+        view = BlogAPI.as_view({
+            'get': 'list'
+        })
+        response = view(request)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 1 == response.data['count']
+        assert keyword == response.data['results'][0]['title']
+
+    def test_get_blog_list(self):
+        """
+        Tests to get Blog Posts Successfully.
+        """
+        url = '/api/blog/'
+        request = self.factory.get(url)
+        view = BlogAPI.as_view({
+            'get': 'list'
+        })
+        response = view(request)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 0 == response.data['count']
+
+    def test_blog_upvote(self):
+        """
+        Tests Upvote functionality.
+        """
+        self._create_blog()
+
+        user = User.objects.get(username='test')
+        blog = Blog.objects.get()
+        url = '/api/blog/'
+        request = self.factory.get(url)
+        force_authenticate(request, user=user)
+        view = BlogAPI.as_view({
+            'get': 'upvote'
+        })
+        response = view(request, slug=blog.slug)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 'You have successfully up voted this blog post.' == response.data
+
+        response = view(request, slug=blog.slug)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 'You have already up voted this blog post.' == response.data
+
+    def test_blog_downvote(self):
+        """
+        Tests Downvote functionality.
+        """
+        self._create_blog()
+
+        user = User.objects.get(username='test')
+        blog = Blog.objects.get()
+        url = '/api/blog/'
+        request = self.factory.get(url)
+        force_authenticate(request, user=user)
+        view = BlogAPI.as_view({
+            'get': 'downvote'
+        })
+        response = view(request, slug=blog.slug)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 'You have successfully down voted this blog post.' == response.data
+
+        response = view(request, slug=blog.slug)
+
+        assert status.HTTP_200_OK == response.status_code
+        assert 'You have already down voted this blog post.' == response.data
+
+    def test_post_a_comment(self):
+        """
+        Tests a comment is successfully posted.
+        """
+        response = self._create_blog()
+
+        user = User.objects.get(username='test')
+        blog = response.data['url']
+        comment_content = {
+            'blog': blog,
+            'description': 'This is a Test Comment.'
+        }
+        url = '/api/comment/'
+        request = self.factory.post(url, comment_content)
+        force_authenticate(request, user=user)
+        view = CommentAPI.as_view({
+            'post': 'create'
+        })
+        response = view(request)
+
+        assert status.HTTP_201_CREATED == response.status_code
+        assert comment_content['description'] == response.data['description']
+        assert response.data['parent'] is None
+
+        reply_content = {
+            'blog': blog,
+            'description': 'This is a Test Reply.',
+            'parent': response.data['id']
+        }
+        request = self.factory.post(url, reply_content)
+        force_authenticate(request, user=user)
+        response = view(request)
+
+        assert status.HTTP_201_CREATED == response.status_code
+        assert reply_content['description'] == response.data['description']
+        assert reply_content['parent'] == response.data['parent']
